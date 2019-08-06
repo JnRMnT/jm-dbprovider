517c01acee8c6975e68db2d59414dc91e5557580
Ozan KANIK <l1110018@std.yildiz.edu.tr>
2019-08-07 00:58:20 +0200

    --exit flag passed to mocha on Tests

--- JMDBProvider/test/test.js
+++ JMDBProvider/test/test.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference path="../Scripts/typings/index.d.ts" />
 var assert = require("assert");
+var app_1 = require("../app");
 var jmdbProvider = require('../app');
 var testDbConfig = {
     serverName: "ds135039.mlab.com:35039",
@@ -14,8 +15,8 @@ describe("Main Tests", function () {
         try {
             jmdbProvider.connect(testDbConfig.userName, testDbConfig.password, testDbConfig.databaseName, testDbConfig.serverName).then(function () {
                 assert.ok(true, "Database connection successful");
-                done();
                 jmdbProvider.close();
+                done();
             }, function () {
                 done("Database connection failed");
             });
@@ -25,116 +26,119 @@ describe("Main Tests", function () {
             assert.ok(false, "Database connection failed");
         }
     });
-    //it("Insert - Delete Test", (done) => {
-    //    try {
-    //        jmdbProvider.connect(testDbConfig.userName, testDbConfig.password, testDbConfig.databaseName, testDbConfig.serverName).then(function () {
-    //            return jmdbProvider.insert("test", { testProperty: true }).then(function () {
-    //                return jmdbProvider.find("test", undefined, { testProperty: true }).then(function (data) {
-    //                    assert.equal(data.length, 1, "Insert operation failed");
-    //                    return jmdbProvider.deleteMany("test", { testProperty: true }).then(function () {
-    //                        return jmdbProvider.find("test").then(function (data) {
-    //                            assert.equal(data.length, 0, "Delete operation failed");
-    //                            jmdbProvider.close();
-    //                            done();
-    //                        });
-    //                    }, function () {
-    //                        done("Delete operation rejected");
-    //                    });
-    //                }, function () {
-    //                    done("Insert operation rejected");
-    //                });
-    //            });
-    //        }, function () {
-    //            done("Database connection failed");
-    //        });
-    //    } catch (ex) {
-    //        done("Insert-Delete operation failed");
-    //    }
-    //});
-    //it("Limit Test", (done) => {
-    //    try {
-    //        jmdbProvider.connect(testDbConfig.userName, testDbConfig.password, testDbConfig.databaseName, testDbConfig.serverName).then(function () {
-    //            var insertObjects = [];
-    //            for (var i = 0; i < 100; i++) {
-    //                insertObjects.push({ testProperty: true });
-    //            }
-    //            return jmdbProvider.insertMany("test", insertObjects).then(function () {
-    //                return jmdbProvider.find("test").then(function (data) {
-    //                    assert.equal(data.length, 100, "Insert operation failed");
-    //                    return jmdbProvider.find("test", 10).then(function (data) {
-    //                        assert.equal(data.length, 10, "Limit operation failed");
-    //                        return jmdbProvider.deleteMany("test", undefined).then(function () {
-    //                            return jmdbProvider.find("test").then(function (data) {
-    //                                assert.equal(data.length, 0, "Delete operation failed");
-    //                                jmdbProvider.close();
-    //                                done();
-    //                            }, function () {
-    //                                done("find after delete operation rejected");
-    //                            });
-    //                        }, function () {
-    //                            done("Delete operation rejected");
-    //                        });
-    //                    }, function () {
-    //                        done("Limited Find operation rejected");
-    //                    });
-    //                }, function () {
-    //                    done("Find operation rejected");
-    //                });
-    //            }, function () {
-    //                done("Insert operation rejected");
-    //            });
-    //        }, function () {
-    //            done("Database connection failed");
-    //        });
-    //    } catch (ex) {
-    //        done("Limit test failed");
-    //    }
-    //});
-    //it("Sorting Test", (done) => {
-    //    try {
-    //        jmdbProvider.connect(testDbConfig.userName, testDbConfig.password, testDbConfig.databaseName, testDbConfig.serverName).then(function () {
-    //            let finishTest = function (error?: string) {
-    //                jmdbProvider.deleteMany("test", undefined).then(function () {
-    //                    jmdbProvider.close();
-    //                    done(error);
-    //                }, function () {
-    //                    jmdbProvider.close();
-    //                    done("Cleanup error");
-    //                });
-    //            }
-    //            var multipleInsert = [];
-    //            for (var i = 0; i < 100; i++) {
-    //                multipleInsert.push({ sortId: i });
-    //            }
-    //            return jmdbProvider.insertMany("test", multipleInsert).then(function () {
-    //                return jmdbProvider.find("test", undefined, undefined, [{
-    //                    fieldName: "sortId", direction: SortDirection.Descending
-    //                }]).then(function (descendingSortedData) {
-    //                    assert.equal(descendingSortedData[0].sortId, 99, "Descending Sort test failed");
-    //                    return jmdbProvider.find("test", undefined, undefined, [{
-    //                        fieldName: "sortId", direction: SortDirection.Ascending
-    //                    }]).then(function (ascendingSortedData) {
-    //                        assert.equal(ascendingSortedData[0].sortId, 0, "Ascending Sort test failed");
-    //                        return jmdbProvider.delete("test", undefined).then(function () {
-    //                            finishTest();
-    //                        }, function () {
-    //                            finishTest("Delete all operation failed");
-    //                        });
-    //                    }, function () {
-    //                        finishTest("Find ascending sorted operation failed");
-    //                    });
-    //                }, function () {
-    //                    finishTest("Find descending sorted operation failed");
-    //                });
-    //            }, function () {
-    //                finishTest("Multiple insertion operation failed");
-    //            });
-    //        }, function () {
-    //            done("Database connection failed");
-    //        });
-    //    } catch (ex) {
-    //        assert.ok(false, "Sort test failed");
-    //    }
-    //});
+    it("Insert - Delete Test", function (done) {
+        try {
+            jmdbProvider.connect(testDbConfig.userName, testDbConfig.password, testDbConfig.databaseName, testDbConfig.serverName).then(function () {
+                return jmdbProvider.insert("test", { testProperty: true }).then(function () {
+                    return jmdbProvider.find("test", undefined, { testProperty: true }).then(function (data) {
+                        assert.equal(data.length, 1, "Insert operation failed");
+                        return jmdbProvider.deleteMany("test", { testProperty: true }).then(function () {
+                            return jmdbProvider.find("test").then(function (data) {
+                                assert.equal(data.length, 0, "Delete operation failed");
+                                jmdbProvider.close();
+                                done();
+                            });
+                        }, function () {
+                            done("Delete operation rejected");
+                        });
+                    }, function () {
+                        done("Insert operation rejected");
+                    });
+                });
+            }, function () {
+                done("Database connection failed");
+            });
+        }
+        catch (ex) {
+            done("Insert-Delete operation failed");
+        }
+    });
+    it("Limit Test", function (done) {
+        try {
+            jmdbProvider.connect(testDbConfig.userName, testDbConfig.password, testDbConfig.databaseName, testDbConfig.serverName).then(function () {
+                var insertObjects = [];
+                for (var i = 0; i < 100; i++) {
+                    insertObjects.push({ testProperty: true });
+                }
+                return jmdbProvider.insertMany("test", insertObjects).then(function () {
+                    return jmdbProvider.find("test").then(function (data) {
+                        assert.equal(data.length, 100, "Insert operation failed");
+                        return jmdbProvider.find("test", 10).then(function (data) {
+                            assert.equal(data.length, 10, "Limit operation failed");
+                            return jmdbProvider.deleteMany("test", undefined).then(function () {
+                                return jmdbProvider.find("test").then(function (data) {
+                                    assert.equal(data.length, 0, "Delete operation failed");
+                                    jmdbProvider.close();
+                                    done();
+                                }, function () {
+                                    done("find after delete operation rejected");
+                                });
+                            }, function () {
+                                done("Delete operation rejected");
+                            });
+                        }, function () {
+                            done("Limited Find operation rejected");
+                        });
+                    }, function () {
+                        done("Find operation rejected");
+                    });
+                }, function () {
+                    done("Insert operation rejected");
+                });
+            }, function () {
+                done("Database connection failed");
+            });
+        }
+        catch (ex) {
+            done("Limit test failed");
+        }
+    });
+    it("Sorting Test", function (done) {
+        try {
+            jmdbProvider.connect(testDbConfig.userName, testDbConfig.password, testDbConfig.databaseName, testDbConfig.serverName).then(function () {
+                var finishTest = function (error) {
+                    jmdbProvider.deleteMany("test", undefined).then(function () {
+                        jmdbProvider.close();
+                        done(error);
+                    }, function () {
+                        jmdbProvider.close();
+                        done("Cleanup error");
+                    });
+                };
+                var multipleInsert = [];
+                for (var i = 0; i < 100; i++) {
+                    multipleInsert.push({ sortId: i });
+                }
+                return jmdbProvider.insertMany("test", multipleInsert).then(function () {
+                    return jmdbProvider.find("test", undefined, undefined, [{
+                            fieldName: "sortId", direction: app_1.SortDirection.Descending
+                        }]).then(function (descendingSortedData) {
+                        assert.equal(descendingSortedData[0].sortId, 99, "Descending Sort test failed");
+                        return jmdbProvider.find("test", undefined, undefined, [{
+                                fieldName: "sortId", direction: app_1.SortDirection.Ascending
+                            }]).then(function (ascendingSortedData) {
+                            assert.equal(ascendingSortedData[0].sortId, 0, "Ascending Sort test failed");
+                            return jmdbProvider.delete("test", undefined).then(function () {
+                                finishTest();
+                            }, function () {
+                                finishTest("Delete all operation failed");
+                            });
+                        }, function () {
+                            finishTest("Find ascending sorted operation failed");
+                        });
+                    }, function () {
+                        finishTest("Find descending sorted operation failed");
+                    });
+                }, function () {
+                    finishTest("Multiple insertion operation failed");
+                });
+            }, function () {
+                done("Database connection failed");
+            });
+        }
+        catch (ex) {
+            assert.ok(false, "Sort test failed");
+        }
+    });
 });
 //# sourceMappingURL=test.js.map
